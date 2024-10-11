ea81e20872b939f5e787a653d59340b468b7876b
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-10-11 18:20:04 -0300

    feat: set linters and react navigation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,49 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  plugins: ['import'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'import/order': [
+          'error',
+          {
+            groups: ['external', 'builtin', 'internal', 'parent', 'sibling'],
+            pathGroups: [
+              {
+                pattern: 'react+(|-native)',
+                group: 'external',
+                position: 'before',
+              },
+              {
+                pattern: '@+(routes|screens|components|hooks|theme)',
+                group: 'internal',
+                position: 'before',
+              },
+              {
+                pattern: './',
+                group: 'internal',
+                position: 'before',
+              },
+            ],
+            pathGroupsExcludedImportTypes: ['react+(|-native)'],
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+          },
+        ],
+        '@typescript-eslint/no-unused-vars': 'warn',
+        'react-native/no-inline-styles': 'off',
+        'linebreak-style': 'off',
+        'prettier/prettier': [
+          'error',
+          {
+            endOfLine: 'auto',
+          },
+        ],
+      },
+    },
+  ],
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: '.',
+          alias: {
+            '@components': './src/components',
+            '@domain': './src/domain',
+            '@hooks': './src/hooks',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@theme': './src/theme',
+          },
+        },
+      ],
+    ],
 };
