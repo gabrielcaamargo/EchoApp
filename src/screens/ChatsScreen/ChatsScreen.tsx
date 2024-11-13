5becee03a69be983008f9f0d4b46a4fdaeec2021
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-11-13 08:01:30 -0300

    feat: create mmkv storage

--- src/screens/ChatsScreen/ChatsScreen.tsx
+++ src/screens/ChatsScreen/ChatsScreen.tsx
@@ -1,14 +1,6 @@
-import React, {useEffect} from 'react';
-import {database} from '@database';
-import {getLocales} from 'react-native-localize';
+import React from 'react';
 import {Box, Screen} from '@components';
 export function ChatsScreen() {
-	useEffect(() => {
-		(async function () {
-			console.log('Db:::', database.get('language'));
-		})();
-	}, []);
-
 	return (
 		<Screen>
 			<Box />
