80316f262953b0f076df66970f9c4b837cee2c92
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-11-13 08:01:57 -0300

    feat: implement mmkv storage

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,7 +1,9 @@
+import {MMKVStorage} from './implementation/MMKVStorage';
+
 export interface Storage {
 	getItem: <T = unknown>(key: string) => Promise<T | null>;
 	setItem: <T>(key: string, value: T) => Promise<void>;
 	removeItem: (key: string) => Promise<void>;
 }
 
-export let storage: Storage;
+export let storage: Storage = MMKVStorage;
